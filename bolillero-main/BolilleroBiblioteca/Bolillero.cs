4ecfe73c8b94ddfcc569828d34d83a3238bbe0b8
maaddyy-m <nicoleemartinn712@gmail.com>
2026-05-21 08:43:41 -0300

    Comentarios cs

--- bolillero-main/BolilleroBiblioteca/Bolillero.cs
+++ bolillero-main/BolilleroBiblioteca/Bolillero.cs
@@ -4,14 +4,14 @@ using System.Linq;
 
 namespace Biblioteca
 {
-    public class Bolillero : ICloneable
+    public class Bolillero : ICloneable // Clase de bolillero para clonar 
     {
-        private List<int> bolillasDentro;
-        private List<int> bolillasFuera;
-        private IAzar azar;
-        private readonly object _lock = new object();
+        private List<int> bolillasDentro; // Lista de bolillas que hay adentro
+        private List<int> bolillasFuera; // Lista de bolillas que hay afuera
+        private IAzar azar; // Se elije que azar se va a usar 
+        private readonly object _lock = new object(); // Se usa para crear un objeto de bloqueo que sirve para sincronizar hilos
 
-        public Bolillero(int cantidadBolillas, IAzar azar)
+        public Bolillero(int cantidadBolillas, IAzar azar) // Constructor del bolillero
         {
             this.bolillasDentro = Enumerable.Range(0, cantidadBolillas).ToList();
             this.bolillasFuera = new List<int>();
@@ -20,45 +20,45 @@ namespace Biblioteca
 
         private Bolillero(List<int> bolillasDentro, List<int> bolillasFuera, IAzar azar)
         {
-            this.bolillasDentro = new List<int>(bolillasDentro);
-            this.bolillasFuera = new List<int>(bolillasFuera);
-            this.azar = azar;
+            this.bolillasDentro = new List<int>(bolillasDentro); // Crea una copia de las bolillas de adentro en lista
+            this.bolillasFuera = new List<int>(bolillasFuera); // Crea una copia de las bolillas que salieron
+            this.azar = azar; // Guarda el objeto azar que salio 
         }
 
-        public int SacarBolilla()
+        public int SacarBolilla() // 
         {
-            int indice = azar.ObtenerIndice(bolillasDentro.Count);
-            int bolilla = bolillasDentro[indice];
+            int indice = azar.ObtenerIndice(bolillasDentro.Count); // Pide un numero aleatorio de bolillas dentro
+            int bolilla = bolillasDentro[indice]; // Busca el valor que esta en esa posicion
 
-            bolillasDentro.RemoveAt(indice);
-            bolillasFuera.Add(bolilla);
+            bolillasDentro.RemoveAt(indice); // Elimina la bolilla de la lista de adentro
+            bolillasFuera.Add(bolilla); // Para agregarla en la lista de afuera
 
-            return bolilla;
+            return bolilla; // Devuelve la bolilla
         }
 
-        public void ReingresarBolillas()
+        public void ReingresarBolillas() // Vuelve a meter todas las bolillas adentro nuevamente
         {
-            bolillasDentro.AddRange(bolillasFuera);
-            bolillasFuera.Clear();
+            bolillasDentro.AddRange(bolillasFuera); // Se agrega de una lista a la otra 
+            bolillasFuera.Clear(); // Limpia las bolillas de afuera 
         }
 
-        public int CantidadDentro() => bolillasDentro.Count;
+        public int CantidadDentro() => bolillasDentro.Count; // Csntidad e bolillas adentro 
 
-        public int CantidadFuera() => bolillasFuera.Count;
+        public int CantidadFuera() => bolillasFuera.Count; // Cantidad de bolillas afuera
 
         public bool Jugar(List<int> jugada)
         {
-            foreach (var numero in jugada)
+            foreach (var numero in jugada) // Recorre la jugada 
             {
-                if (SacarBolilla() != numero)
-                    return false;
+                if (SacarBolilla() != numero) // Saca una bolilla y la compara 
+                    return false; // Si sale mal devuelve falso 
             }
-            return true;
+            return true; // Verdadero 
         }
 
-        // El lock garantiza que cada hilo ejecuta su ronda completa (reingresar + jugar)
-        // de forma atómica, sin que otro hilo interfiera con el estado del bolillero.
-        public int JugarNVeces(List<int> jugada, int cantidad)
+        
+    
+        public int JugarNVeces(List<int> jugada, int cantidad) // La cantidad de veces que se puede jugar
         {
             int victorias = 0;
             for (int i = 0; i < cantidad; i++)
@@ -75,7 +75,7 @@ namespace Biblioteca
 
         public object Clone()
         {
-            return new Bolillero(this.bolillasDentro, this.bolillasFuera, this.azar);
+            return new Bolillero(this.bolillasDentro, this.bolillasFuera, this.azar); 
         }
     }
 }
